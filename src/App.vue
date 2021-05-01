d7ceaf740f40c793b713bd93608f20b95a76d4a4
luizcampos331 <luizcampos331@gmail.com>
2021-05-01 12:38:37 -0300

    feat - request to pokeapi by axios

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,24 @@
 </template>
 
 <script>
+import axios from 'axios';
 
 export default {
   name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      pokemons: [],
+    }
+  },
+  created: function() {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then(response => {
+      this.pokemons = response.data.results;
+    }).catch(error => {
+      console.log(error);
+    });
+  }
 }
 </script>
 
